031089eb081a47053dee9406cca33dd8ddafaafe
nathangrinsztajn <ngrinsztajn@gmail.com>
2023-08-21 19:45:42 +0200

    fix: cors

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import requests
 
-# URL = "http://llm4co.cxuf9445.odns.fr/"
-URL = "http://127.0.0.1:5000/"
+URL = "http://llm4co.cxuf9445.odns.fr/"
+# URL = "http://127.0.0.1:5000/"
 
 
 def post_data_to_backend(name, time, performance, function_code):
-    backend_url = URL + "register"
+    backend_url = URL + "register/"
     payload = {
         'name': name,
         'time': time,
