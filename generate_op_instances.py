f3ea73bcf9e05bb4be3423219463003cee3f891b
nathangrinsztajn <ngrinsztajn@gmail.com>
2023-08-23 12:03:47 +0200

    update generation

--- generate_op_instances.py
+++ generate_op_instances.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def generate_instance(size, prize_type):
+def generate_instance(size):
     MAX_LENGTHS = {
         20: 2.,
         50: 3.,
@@ -10,15 +10,7 @@ def generate_instance(size, prize_type):
 
     loc = np.random.uniform(0, 1, (size, 2))
     depot = np.random.uniform(0, 1, 2)
-
-    if prize_type == 'const':
-        prize = np.ones(size)
-    elif prize_type == 'unif':
-        prize = (1 + np.random.randint(0, 100, size=size)) / 100.
-    else:  # Based on distance to depot
-        assert prize_type == 'dist'
-        prize_ = np.linalg.norm(depot[None, :] - loc, axis=-1)
-        prize = (1 + (prize_ / np.max(prize_) * 99).astype(int)) / 100.
+    prize = (1 + np.random.randint(0, 100, size=size)) / 100.
 
     return {
         'loc': loc,
